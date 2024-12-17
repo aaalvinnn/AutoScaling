ef6df9486e5ff94d70f31764d77d616a76827eb1
zswb209 <2869242945@qq.com>
2024-12-17 20:49:03 +0800

    添加randomagent用于测试

--- methods/RandomScaling.py
+++ methods/RandomScaling.py
@@ -1,13 +1,28 @@
 import random
 from env import environment
+from env import config
+import numpy as np
 
 
-class RandomScalingEnv(environment.DataCenterEnvironment):
-    def __init__(self, env_config):
-        super().__init__(env_config)
+class RandomScalingAgent(object):
+    def __init__(self, env: environment.DataCenterEnvironment):
+        self.env = env
+        self.actoin_space_dim = np.array([env.server_node_nums, env.ms_nums, env.config.max_instance_update_num])
         pass
 
-
-
-if __name__ == '__main__':
-    env = environment.DataCenterEnvironment()
+    def get_action(self, state):
+        """
+        有条件的随机选择一个action
+        return: np.array([node_id, ms_id, ms_image_nums])
+        """
+        done = False
+        (node_id, ms_id, ms_image_nums) = (0, 0, 0)
+        while not done:
+            node_id = random.randint(0, self.actoin_space_dim[0] - 1)
+            ms_id = random.randint(0, self.actoin_space_dim[1] - 1)
+            ms_image_nums = random.randint(-self.actoin_space_dim[2], self.actoin_space_dim[2])
+            # 若不是 卸载掉了一个用户需要且数据中心仅有最后1个实例的微服务
+            if not (self.env._is_ms_needed(ms_id) and self.env.ms_image_list[ms_id] <= -ms_image_nums):
+                done = True
+            
+        return np.array([node_id, ms_id, ms_image_nums])
---
+++ test.py
@@ -0,0 +1,37 @@
+from env import config, environment, loghelper
+from methods import RandomScaling
+
+
+class TrainAndTestHelper(object):
+    def __init__(self, env: environment.DataCenterEnvironment, agent: RandomScaling.RandomScalingAgent, logger: loghelper.LogHelper):
+        self.env = env
+        self.agent = agent
+        self.logger = logger
+
+    def test(self):
+        total_reward = 0
+        done = False
+        state = self.env.reset()
+
+        while(not done):
+            action = self.agent.get_action(state)
+            next_state, reward, done, info = self.env.step(action)
+            total_reward += reward
+            state = next_state
+            for key in info:
+                self.logger.record(key, info[key])
+
+        print("total_reward: ", total_reward)
+        self.logger.visualize()
+
+
+if __name__ == '__main__':
+    env_config = config.EnvConfig()
+    env = environment.DataCenterEnvironment(env_config)
+    agent = RandomScaling.RandomScalingAgent(env)
+    logger = loghelper.LogHelper()
+    train_and_test_helper = TrainAndTestHelper(env, agent, logger)
+    train_and_test_helper.test()
+
+        
+
