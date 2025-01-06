a60df2d89623800a7535a80a0d3fe22e48bd57e1
zswb209 <2869242945@qq.com>
2025-01-06 13:55:48 +0800

    更新随机动作选择算法

--- methods/RandomScaling.py
+++ methods/RandomScaling.py
@@ -7,22 +7,39 @@ import numpy as np
 class RandomScalingAgent(object):
     def __init__(self, env: environment.DataCenterEnvironment):
         self.env = env
-        self.actoin_space_dim = np.array([env.server_node_nums, env.ms_nums, env.config.max_instance_update_num])
+        self.actoin_space_dim = np.array([env.server_node_nums, env.ms_nums, env.config.max_instance_update_num*2+1])
+        self.offset = self.env.config.max_instance_update_num
         pass
 
     def get_action(self, state):
         """
         有条件的随机选择一个action
-        return: np.array([node_id, ms_id, ms_image_nums])
+        return: np.array([node_id, ms_id, delta])
         """
         done = False
-        (node_id, ms_id, ms_image_nums) = (0, 0, 0)
+        (node_id, ms_id, delta) = (0, 0, 0)
         while not done:
+            # 随机抽样
             node_id = random.randint(0, self.actoin_space_dim[0] - 1)
             ms_id = random.randint(0, self.actoin_space_dim[1] - 1)
-            ms_image_nums = random.randint(-self.actoin_space_dim[2], self.actoin_space_dim[2])
-            # 若不是 卸载掉了一个用户需要且数据中心仅有最后1个实例的微服务
-            if not (self.env._is_ms_needed(ms_id) and self.env.ms_image_list[ms_id] <= -ms_image_nums):
+            delta = random.randint(0, self.actoin_space_dim[2] - 1)
+            # 计算动作执行后的服务强度
+            lamda = state[3][ms_id][0] * self.env.config.estimated_max_lamda
+            mu = self.env.MS_list[ms_id].mu * (self.env.ms_image_list[ms_id]+delta-self.offset)
+            ro = lamda / (mu + 1e-6)
+
+            # 若节点没有这个实例or资源不足
+            node = self.env.Node_list[node_id]
+            ms = self.env.MS_list[ms_id]
+            if not node.is_resource_enough(ms, delta-self.offset):
+                done = False
+            # 若弹性伸缩后该微服务队列拥塞
+            elif ro >= 1:
+                done = False
+            else:
                 done = True
+
+        if (self.env._get_congested_queue_nums() > 0):
+            pass
             
-        return np.array([node_id, ms_id, ms_image_nums])
+        return np.array([node_id, ms_id, delta])
